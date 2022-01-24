fba2bc97b77bb1f3da6a96d894316ea098bd38c5
Andres Pulecio <andres.pulecio@blackboard.com>
2022-01-23 22:04:29 -0500

    feat: count down 1 minute

--- pomodoro-window.py
+++ pomodoro-window.py
@@ -7,9 +7,20 @@ import time
 
 x = 5
 yellow = "#f7f5dd"
+reps = 0
+work_time = 60
 
+def count_down(count):
+    global timer
+    minutes = count // 60
+    seconds = count % 60 
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    timer = window.after(1000, count_down, count -1)
+def start_timer():
+    global reps
+    reps += 1
+    count_down(work_time)
 
-# time_input = 10
 #Window
 window = Tk()
 window.title("Pomodoro")
@@ -20,27 +31,16 @@ canvas = Canvas(width=400, height=400, bg=yellow, highlightthickness=0)
 tomato_img = PhotoImage(file = "./pomodoro-clock/resources/tomato-image.png")
 canvas.create_image(190, 190, image=tomato_img)
 
-#timer
-timer_text = canvas.create_text(200, 250, text = "00:00", fill = "white", font=("Courier", 50, "bold"))
-canvas.grid(column=1, row=1)
-min = 60
-sec = 60
-canvas.itemconfig(timer_text, text=f"{min}:{sec}")
-
-
 #Buttons
 button_start = ttk.Button(text="Start") 
 button_start.place(x = 100, y = 360)
 button_restart = ttk.Button(text="Restart")
 button_restart.place(x = 200, y = 360)
 
-# minutes = time_input // 60
-# seconds = time_input % 60 
-# timer = '{:02d}:{:02d}'.format(minutes,seconds)
-# label = Label(window, text= timer)
-# label.config(bg=yellow)
-# label.pack()
-# time.sleep(1)
-# time_input -= 1
+#timer
+timer_text = canvas.create_text(200, 250, text = "00:00", fill = "white", font=("Courier", 50, "bold"))
+canvas.grid(column=1, row=1)
+
+start_timer()
 
 window.mainloop()
