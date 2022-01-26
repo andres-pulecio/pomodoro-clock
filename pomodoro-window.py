a1299bc70b9c53a8792242c9bf196adafe401d52
Andres Pulecio <andres.pulecio@blackboard.com>
2022-01-25 21:44:49 -0500

    fix: Count in negative

--- pomodoro-window.py
+++ pomodoro-window.py
@@ -1,46 +1,57 @@
 from cgitb import text
 from curses import window
+from tkinter import ttk, Tk, Canvas, PhotoImage
 from tkinter import *
-from tkinter import ttk
-import math
-import time
+
 
 x = 5
 yellow = "#f7f5dd"
 reps = 0
-work_time = 60
+work_time = 3
+rest_time = 5
+timer = NONE
 
+# trasform the value in the timer 
 def count_down(count):
-    global timer
     minutes = count // 60
     seconds = count % 60 
-    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
-    timer = window.after(1000, count_down, count -1)
+    timer_format = '{:02d}:{:02d}'.format(minutes,seconds)
+    canvas.itemconfig(timer_text, text=f"{timer_format}")
+   
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count -1)
+    else:
+        count = 0
+        
+def restart_timer():
+    global reps
+    reps = 0
+    count_down(0)
+    
 def start_timer():
     global reps
     reps += 1
     count_down(work_time)
 
-#Window
+#Create the interface Window 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=10, pady=10, bg=yellow)
 
-#Tomato image
+#Create and configure Tomato image
 canvas = Canvas(width=400, height=400, bg=yellow, highlightthickness=0)
 tomato_img = PhotoImage(file = "./pomodoro-clock/resources/tomato-image.png")
 canvas.create_image(190, 190, image=tomato_img)
 
-#Buttons
-button_start = ttk.Button(text="Start") 
-button_start.place(x = 100, y = 360)
-button_restart = ttk.Button(text="Restart")
-button_restart.place(x = 200, y = 360)
+#Create and configure Buttons
+start_button = ttk.Button(text="Start", command = start_timer) 
+start_button.place(x = 100, y = 360)
+restart_button = ttk.Button(text="Restart", command = restart_timer)
+restart_button.place(x = 200, y = 360)
 
-#timer
+#Initial values for timer
 timer_text = canvas.create_text(200, 250, text = "00:00", fill = "white", font=("Courier", 50, "bold"))
 canvas.grid(column=1, row=1)
-
-start_timer()
 
 window.mainloop()
